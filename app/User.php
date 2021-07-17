ba802d4acff4d293e7c2072f22c2d78a94885876
safwendammak <safwen.dammak@sesame.com.tn>
2021-07-18 00:21:58 +0100

    Add follows list

--- app/User.php
+++ app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -45,5 +44,15 @@ class User extends Authenticatable
     public function timeline()
     {
         return Tweet::where('user_id', $this->id)->latest()->get();
+    }
+
+    public function follow(User $user)
+    {
+        $this->follows()->save($user);
+    }
+
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
     }
 }
