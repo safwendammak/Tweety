1ee8e454bc2f01fc82eaba24afe7417272b2ba94
safwendammak <safwen.dammak@sesame.com.tn>
2021-07-18 18:05:52 +0100

    Change sections & yield to components

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,6 +1,4 @@
-@extends('components.app')
-
-@section('content')
+<x-app>
     <header class="mb-6 relative">
         <img src="/images/default-profile-banner.jpg" alt="" class="mb-2">
 
@@ -31,4 +29,4 @@
     @include ('_timeline', [
     'tweets'=>$user->tweets
     ])
-@endsection
+</x-app>
